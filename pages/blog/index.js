b35b9b9a14ca4e7915528901e7b74b262eadae78
Zehra <zehraviplus@gmail.com>
2022-01-13 01:34:16 +0300

    new commit

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -4,23 +4,22 @@ import { getAllNodes } from "next-mdx/server";
 
 
 function BlogPage({ posts }) {
-  console.log(posts);
-    return <div className="site-container">
+   return <div className="site-container">
       <div className="space-y-7">
       {posts.map(post=> {
-        return <article key={post.url}>
-          <h2 className="h-10 text-xl font-mono underline  underline-offset-2 ">
-            <Link href={post.url}>
-              <a>{post.frontMatter.title}</a>
-            </Link>
-          </h2>
-          <p>{post.frontMatter.excerpt}</p>
-          <div className=" text-slate-400">
+        return(
+           <article key={post.url}>
+            <h2 className="h-10 text-xl font-mono underline  underline-offset-2 ">
+             <Link href={post.url}>
+               <a>{post.frontMatter.title}</a>
+             </Link>
+           </h2>
+           <p>{post.frontMatter.excerpt}</p>
+           <div className=" text-slate-400">
             <span>{post.frontMatter.date}</span>
-            
           </div>
         </article>
-      
+        )
       })}
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,23 @@
-function HomePage() {
-    return (
-  
-     <div className=" bg-lightest">
-     <div className="site-container">
-     <div className="space-y-5">
-     <h1 className="text-2xl font-semibold ">
-        Merhabalaar! Ben Özlem Zehra Dişkaya. </h1>
-      <p >
-        Bursa'da yaşıyorum, tercümanlık yapmaktayım. Yazılım yolculuğuma kısa bir süre önce başladım. Yolculuğumu ve ilerlememi kayıt altına alıp sizlerle paylaşmak istiyorum. Bu sebeple elle tutulur ilk projeme kendi kişisel blog sayfamı yazarak başlıyorum. 
-      </p>
+function HomePage(){
+  return(
+    <>
+     <div className="bg-lightest">
+       <div className="site-container">
+         <div className="space-y-5">
+           <h1 className="text-2xl font-semibold"></h1>
 
+           <p>
+           Bursa'da yaşıyorum, tercümanlık yapmaktayım. Yazılım yolculuğuma kısa bir süre önce başladım. Yolculuğumu ve ilerlememi kayıt altına alıp sizlerle paylaşmak istiyorum. Bu sebeple elle tutulur ilk projeme kendi kişisel blog sayfamı yazarak başlıyorum. 
+           </p>
+         </div>
        </div>
-  
-      </div>
-      <div className="site-4xl-container mt-20 "><img src="/heyhey.gif" alt=""/> </div>
-      </div>
-     
-    )
-  }
-  
-  export default HomePage
+
+       <div className="site-4xl-container mt-20 "><img src="/heyhey.gif" alt=""/> </div>
+
+
+     </div>
+    </>
+  )
+}
+
+export default HomePage
